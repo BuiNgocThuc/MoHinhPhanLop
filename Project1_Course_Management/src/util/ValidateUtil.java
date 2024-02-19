c344f617cf2f13ac0bac18413981b9bab4e6c929
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-20 01:31:26 +0700

    docs: add study case of try-catch exception

--- Project1_Course_Management/src/util/ValidateUtil.java
+++ Project1_Course_Management/src/util/ValidateUtil.java
@@ -15,4 +15,47 @@ public class ValidateUtil {
 
         return matchFound;
     }
+// Studycase of Try-Catch
+//
+//    public static void someMethodInBLL(String name) throws ValidationException, Throwable {
+//        try {
+//            // check validate data
+//            if (!isName(name)) {
+//                throw new ValidationException("Field name is incorrect!");
+//            }
+//
+//            // Do some stuff 
+//            if (Math.random() * 10 > 10) {
+//                throw new Exception("Duplicate account");
+//            }
+//        } catch (Exception e) {
+//            throw e;
+//        }
+//    }
+//
+//    public static void someMethodInUI() {
+//        String name = "Tuan"; // get from field
+//
+//        try {
+//            someMethodInBLL(name);
+//
+//            System.out.println("Done!");
+//        } catch (ValidationException e) {
+//            // Do some stuff with input field
+//            System.out.println(e.getMessage());
+//        } catch (Exception e) {
+//            System.out.println("Something went wrong!");
+//        } catch (Throwable ex) {
+//            Logger.getLogger(ValidateUtil.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) throws ValidationException {
+//        try {
+//            someMethodInUI();
+//        } catch (Exception e) {
+//            // 
+//        }
+//    }
 }
