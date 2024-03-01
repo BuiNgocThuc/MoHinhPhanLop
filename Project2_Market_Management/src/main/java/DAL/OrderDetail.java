786de12b24b793580e82ddba3dedcced867a401c
tikjuti <tangquoctuan2003@gmail.com>
2024-03-01 09:30:13 +0700

    config hibernate file

--- Project2_Market_Management/src/main/java/DAL/OrderDetail.java
+++ Project2_Market_Management/src/main/java/DAL/OrderDetail.java
@@ -1,6 +1,62 @@
 
 package DAL;
 
-public class OrderDetail {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orderdetail")
+public class OrderDetail implements Serializable{
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "OrderID")
+    private Order order;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "VegetableID")
+    private Vegetable vegetable;
+    @Column(name = "Quantity")
+    private int quantity;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Vegetable getVegetable() {
+        return vegetable;
+    }
+
+    public void setVegetable(Vegetable vegetable) {
+        this.vegetable = vegetable;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    @Column(name = "Price")
+    private Double price;
+    
+    
     
 }
