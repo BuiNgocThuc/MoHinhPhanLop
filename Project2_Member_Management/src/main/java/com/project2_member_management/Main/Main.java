8196073d5b0d4c5c5b42d51728d77d773fa72554
tikjuti <tangquoctuan2003@gmail.com>
2024-04-01 16:50:16 +0700

    update statistic device

--- Project2_Member_Management/src/main/java/com/project2_member_management/Main/Main.java
+++ Project2_Member_Management/src/main/java/com/project2_member_management/Main/Main.java
@@ -11,10 +11,9 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) throws ParseException {
-        System.out.println("Hello World!");
         Logger.getLogger("org.hibernate").setLevel(Level.OFF);
         DeviceBLL dvBLL = new DeviceBLL();
-        List<Device> devices = dvBLL.timeStatisticIsBorrowing("2024-02-10", "2024-02-15");
+        List<Device> devices = dvBLL.statisticDeviceIsBorrowing(null, "2024-04-25");
         
         for(Device d : devices) {
             System.out.println("id: " + d.getId());
