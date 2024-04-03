49334514c0670acdccd2ddc375de3c20614f890e
Văn <95088369+maivantai2003@users.noreply.github.com>
2024-04-03 22:39:09 +0700

    update main

--- Project2_Member_Management/src/main/java/com/project2_member_management/Main/Main.java
+++ Project2_Member_Management/src/main/java/com/project2_member_management/Main/Main.java
@@ -4,12 +4,17 @@
 
 package com.project2_member_management.Main;
 
+import POJOs.Device;
+import Utils.hibernateUtil;
+import jakarta.persistence.Query;
+import java.util.List;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author buing
  */
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
