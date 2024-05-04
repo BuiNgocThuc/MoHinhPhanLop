efc794b142801d072195de4c9531e99a29db726e
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-05 03:57:08 +0700

    chore: remove ignored files to avoid conflict in future

--- Project3_Member_Management_SpringBoot/target/classes/static/js/reservation.js
+++
@@ -1,8 +0,0 @@
-
-let buttons = document.querySelectorAll('button#reservationButton');
-buttons.forEach((button, _) => {
-    button.addEventListener('click', function () {
-        alert('hello world');
-        console.log('run')
-    });
-});
