526ebbc7895ae649cc7dedc7afd3bb39b30f5b5b
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-05 04:58:42 +0700

    feat: add `AuthRequire` and `AuthInterceptor`

--- Project3_Member_Management_SpringBoot/src/main/java/com/project3/Member_Management_SpringBoot/annotation/AuthRequire.java
+++ Project3_Member_Management_SpringBoot/src/main/java/com/project3/Member_Management_SpringBoot/annotation/AuthRequire.java
@@ -7,7 +7,6 @@ import java.lang.annotation.Target;
 
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RoleRequire {
+public @interface AuthRequire {
 
-    String[] value() default {};
 }
