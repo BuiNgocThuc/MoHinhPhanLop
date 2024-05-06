f09da7a68df4102dfe25c9e0bc166208ea9b49c2
PhatJack <tienphatng.693@gmail.com>
2024-05-06 13:27:15 +0700

    feat: add active class

--- Project3_Member_Management_SpringBoot/src/main/resources/static/js/admin.js
+++ Project3_Member_Management_SpringBoot/src/main/resources/static/js/admin.js
@@ -1,5 +1,15 @@
 const hamBurger = document.querySelector(".toggle-btn");
-
 hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
+$(document).ready(function () {
+    $('.sidebar-item .sidebar-link').on("click", function (e) {
+        e.preventDefault(); // Prevent default link behavior
+
+        // Remove 'active' class from all sidebar links
+        $('.sidebar-link').removeClass('active');
+
+        // Add 'active' class to the clicked sidebar link
+        $(this).addClass('active');
+    });
 });
