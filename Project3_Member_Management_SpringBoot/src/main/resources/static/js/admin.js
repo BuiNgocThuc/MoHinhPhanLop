c34001c8e7821c04625fd3f4438aafd9941e12e5
PhatJack <tienphatng.693@gmail.com>
2024-05-07 22:49:42 +0700

    feat: fix ui all pages in admin

--- Project3_Member_Management_SpringBoot/src/main/resources/static/js/admin.js
+++ Project3_Member_Management_SpringBoot/src/main/resources/static/js/admin.js
@@ -11,4 +11,15 @@ $(document).ready(function () {
         // Add 'active' class to the clicked sidebar link
         $(this).addClass('active');
     });
+    var path = window.location.pathname;
+    // Loop through each <a> tag in the sidebar
+    $('.sidebar-item .sidebar-link').each(function () {
+        var href = $(this).attr('href');
+        console.log(href);
+        // Check if the href attribute matches the current path
+        if (href == path) {
+            // Add the 'active' class to the parent <li> of the matching <a> tag
+            $(this).addClass('active');
+        }
+    });
 });
