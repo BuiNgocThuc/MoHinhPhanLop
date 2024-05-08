5d4ede67028674e118021f8aa76d5fa8d2dfa5eb
baobui3103 <hongbao2003@gmail.com>
2024-05-08 16:21:52 +0700

    feat: call api

--- Project3_Member_Management_SpringBoot/src/main/java/com/project3/Member_Management_SpringBoot/controller/StatisticsController.java
+++ Project3_Member_Management_SpringBoot/src/main/java/com/project3/Member_Management_SpringBoot/controller/StatisticsController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -24,21 +25,42 @@ public class StatisticsController {
     private final DeviceService deviceService;
     private final DisciplineService disciplineService;
 
-    @GetMapping("/admin/dashboard")
-    public String dashboard(Model theModel) {
-        Integer memberCount = memberService.statisticsMember("CNTT", "Hệ thống thông tin", null, null);
-        theModel.addAttribute("memberData", memberCount);
-        Integer borrowedDeviceData = deviceService.statisticsBorrowedDevice("Micro", null, null);
-        theModel.addAttribute("borrowedDeviceData", borrowedDeviceData);
-        Integer borrowingDeviceData = deviceService.statisticsBorrowingDevice(null, null);
-        theModel.addAttribute("borrowingDeviceData", borrowingDeviceData);
-        Integer resolvedDisciplineData = disciplineService.findByStatus(0);
-        theModel.addAttribute("resolvedDisciplineData", resolvedDisciplineData);
-        Integer totalFineData = disciplineService.findSumFine();
-        theModel.addAttribute("totalFineData", totalFineData);
-        Integer unresolvedDisciplineData = disciplineService.findByStatus(1);
-        theModel.addAttribute("unresolvedDisciplineData", unresolvedDisciplineData);
+    // @GetMapping("/admin/dashboard")
+    // public String dashboard(Model theModel) {
+    // Integer memberCount = memberService.statisticsMember("CNTT", "Hệ thống thông
+    // tin", null, null);
+    // theModel.addAttribute("memberData", memberCount);
+    // Integer borrowedDeviceData = deviceService.statisticsBorrowedDevice("Micro",
+    // null, null);
+    // theModel.addAttribute("borrowedDeviceData", borrowedDeviceData);
+    // Integer borrowingDeviceData = deviceService.statisticsBorrowingDevice(null,
+    // null);
+    // theModel.addAttribute("borrowingDeviceData", borrowingDeviceData);
+    // Integer resolvedDisciplineData = disciplineService.findByStatus(0);
+    // theModel.addAttribute("resolvedDisciplineData", resolvedDisciplineData);
+    // Integer totalFineData = disciplineService.findSumFine();
+    // theModel.addAttribute("totalFineData", totalFineData);
+    // Integer unresolvedDisciplineData = disciplineService.findByStatus(1);
+    // theModel.addAttribute("unresolvedDisciplineData", unresolvedDisciplineData);
+    //
+    // return "admin/dashboard";
+    // }
+    @GetMapping("/admin/device/statictis")
+    public String getDeviceStatistics(
+            @RequestParam("search") String searchDeviceVal,
+            @RequestParam("select") String selectDeviceVal,
+            @RequestParam("startDate") String startDateDevice,
+            @RequestParam("endDate") String endDateDevice) {
+        // Here, you can process the received data, perform database queries,
+        // or any other logic you need for generating statistics
+        System.out.println(searchDeviceVal);
+        System.out.println(selectDeviceVal);
+        System.out.println(startDateDevice);
+        System.out.println(endDateDevice);
 
-        return "admin/dashboard";
+//        // For example, you can create a JSON string manually
+//        String jsonResponse = "{\"search\": \"" + searchDeviceVal + "\", \"select\": \"" + selectDeviceVal + "\", \"startDate\": \"" + startDateDevice + "\", \"endDate\": \"" + endDateDevice + "\"}";
+
+        return "";
     }
 }
